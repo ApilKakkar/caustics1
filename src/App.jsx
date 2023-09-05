f346fa16d40011b07c841abfd3b5845a6e718054
Apil Kakkar <apilkakkar@Apils-MacBook-Air.local>
2023-09-05 20:36:20 +0530

    basic caustics setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Caustic1 from './assets/Caustic1'
+
+function App() {
+  return (
+    <>
+      <Caustic1 />
+    </>
+  )
+}
+
+export default App
